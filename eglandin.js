77e913077ad2f11c01ae3fc9d175cd95b3459105
eglandinselmani <eglandinselmani1@gmail.com>
2025-02-25 20:49:58 +0100

    eg

--- eglandin.js
+++ eglandin.js
@@ -1,6 +1,15 @@
-<<<<<<< Updated upstream:scripts.js
+/*!
+* Start Bootstrap - Agency v7.0.12 (https://startbootstrap.com/theme/agency)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
 window.addEventListener('DOMContentLoaded', event => {
 
+    // Navbar shrink function
     var navbarShrink = function () {
         const navbarCollapsible = document.body.querySelector('#mainNav');
         if (!navbarCollapsible) {
@@ -14,10 +23,13 @@ window.addEventListener('DOMContentLoaded', event => {
 
     };
 
+    // Shrink the navbar 
     navbarShrink();
 
+    // Shrink the navbar when page is scrolled
     document.addEventListener('scroll', navbarShrink);
 
+    //  Activate Bootstrap scrollspy on the main nav element
     const mainNav = document.body.querySelector('#mainNav');
     if (mainNav) {
         new bootstrap.ScrollSpy(document.body, {
@@ -26,7 +38,7 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     };
 
-
+    // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
     const responsiveNavItems = [].slice.call(
         document.querySelectorAll('#navbarResponsive .nav-link')
@@ -40,5 +52,3 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
-=======
->>>>>>> Stashed changes:startbootstrap-agency-gh-pages/js/scripts.js
